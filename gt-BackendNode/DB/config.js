09b4631215e621f737cd7546af735fa57bcc7466
Anikat Kushwaha <anikat.1999.akas@gmail.com>
2024-04-01 20:12:02 +0530

    update

--- gt-BackendNode/DB/config.js
+++ gt-BackendNode/DB/config.js
@@ -1,2 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/GaroHills-tourism");
+// mongoose.connect("mongodb://localhost:27017/GaroHills-tourism");
+mongoose.connect(
+  "mongodb+srv://anikat1999akas:ijV7yeVrbNf8eQHN@cluster0.cim2rsb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/GaroHills-tourism"
+);
